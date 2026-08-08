d0ef803313ed3fd76f08ccb78691073b85529e03
Kristel Oneli Alessandra Canás Hernández <kristelcanas35@gmail.com>
2026-08-08 10:13:06 -0600

    Segunda parte tarea

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, Pressable, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
@@ -13,6 +13,23 @@ export default function Index() {
         Kristel Oneli Alessandra Canás Hernández
       </Text>
 
+      <Text style={styles.carrera}>
+        Ingeniería en Sistemas
+      </Text>
+
+      <Text style={styles.carnet}>
+        Carné: 0907-25-12846
+      </Text>
+
+      <Pressable
+        style={styles.boton}
+        onPress={() => console.log("Ver proyectos")}
+      >
+        <Text style={styles.botonTexto}>
+          Ver proyectos
+        </Text>
+      </Pressable>
+
     </View>
   );
 }
@@ -23,7 +40,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 16,
-    backgroundColor: "#fbf9fb",
+    backgroundColor: "#ecc9f2",
   },
 
   foto: {
@@ -35,6 +52,31 @@ const styles = StyleSheet.create({
 
   nombre: {
     fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  carrera: {
+    fontSize: 16,
+    color: "#555",
+    marginTop: 4,
+  },
+
+  carnet: {
+    fontSize: 14,
+    color: "#888",
+    marginTop: 4,
+    marginBottom: 20,
+  },
+
+  boton: {
+    backgroundColor: "#622cad",
+    padding: 12,
+    borderRadius: 8,
+  },
+
+  botonTexto: {
+    color: "#fff",
+    fontSize: 16,
     fontWeight: "bold",
   },
 });
